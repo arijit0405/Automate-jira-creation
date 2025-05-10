e3eeaab6dce33e516b841da7a374ab1127dc8b99
arijit0405 <arijitchakraborty691@gmail.com>
2025-05-10 13:32:48 +0530

    Update github-jira-ticket.py

--- github-jira-ticket.py
+++ github-jira-ticket.py
@@ -1,69 +1,58 @@
-# This code sample uses the 'requests' library:
-# http://docs.python-requests.org
-import requests
-from requests.auth import HTTPBasicAuth
+import os
 import json
+import requests
 from flask import Flask
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-# Define a route that handles GET requests
 @app.route('/createJira', methods=['POST'])
 def createJira():
-
     url = "https://arijitchakraborty691.atlassian.net/rest/api/3/issue"
 
-   
-  API_Token= ""
-  auth = HTTPBasicAuth("arijitchakraborty691@gmail.com", API_Token )
+    JIRA_USER = os.getenv("JIRA_USER")
+    API_Token = os.getenv("JIRA_API_TOKEN")
+    auth = HTTPBasicAuth(JIRA_USER, API_Token)
 
-  headers = {
-  "Accept": "application/json",
-  "Content-Type": "application/json"
-}
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
 
-payload = json.dumps( {
-  "fields": {
-
-    "description": {
-      "content": [
-        {
-          "content": [
-            {
-              "text": "My First Jira Ticket",
-              "type": "text"
+    payload = json.dumps({
+        "fields": {
+            "project": {
+                "key": "AR"
+            },
+            "summary": "First Jira Ticket Creation",
+            "description": {
+                "type": "doc",
+                "version": 1,
+                "content": [{
+                    "type": "paragraph",
+                    "content": [{
+                        "type": "text",
+                        "text": "My First Jira Ticket"
+                    }]
+                }]
+            },
+            "issuetype": {
+                "id": "10011"
             }
-          ],
-          "type": "paragraph"
         }
-      ],
-      "type": "doc",
-      "version": 1
-    },
-    
-    "issuetype": {
-      "id": "10011"
-    },
-   
-    "project": {
-      "key": "AR"
-    },
-   
-    "summary": "First Jira Ticket Creation"
-   
-  },
-  "update": {}
-} )
+    })
 
-response = requests.request(
-   "POST",
-   url,
-   data=payload,
-   headers=headers,
-   auth=auth
-)
+    response = requests.post(
+        url,
+        headers=headers,
+        data=payload,
+        auth=auth
+    )
 
-return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+    return json.dumps(response.json(), indent=4)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
